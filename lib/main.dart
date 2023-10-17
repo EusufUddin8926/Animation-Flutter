74fa324000ece15bc15afeb054fb493a77605a84
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-17 13:26:23 +0600

    Animated container add

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
 
+
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _isState  = true;
+  var _isState = true;
+  var _isContainerLarge = false;
 
   @override
   Widget build(BuildContext context) {
@@ -37,26 +40,48 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
           title: Text("Animation Demo"), backgroundColor: Colors.blueGrey),
       body: Column(
-        children: [
-          Center(
-            child: AnimatedCrossFade(
-                firstChild: Container(
-                  height: 200,
-                  width: double.infinity,
-                  color: Colors.red,
-                  child: Center(
-                    child: ElevatedButton(onPressed: () {
-                      setState(() {
-                        _isState = false;
-                      });
-                    }, child: Text("Click")),
+          children: [
+            Center(
+              child: AnimatedCrossFade(
+                  firstChild: Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.red,
+                    child: Center(
+                      child: ElevatedButton(onPressed: () {
+                        setState(() {
+                          _isState = false;
+                        });
+                      }, child: Text("Click")),
+                    ),
                   ),
-                ),
-                secondChild: Center(child: Text("Successfully login.")),
-                crossFadeState: _isState?CrossFadeState.showFirst: CrossFadeState.showSecond,
-                duration: Duration(seconds: 1)),
-          ),
-        ]
+                  secondChild: Center(child: Text("Successfully login.")),
+                  crossFadeState: _isState
+                      ? CrossFadeState.showFirst
+                      : CrossFadeState.showSecond,
+                  duration: Duration(seconds: 1)),
+            ),
+            SizedBox(height: 30,),
+            new GestureDetector(
+              onTap: () {
+                setState(() {
+                  if(_isContainerLarge){
+                    _isContainerLarge = false;
+                  }else{
+                    _isContainerLarge = true;
+                  }
+
+                });
+              },
+              child:  AnimatedContainer(
+                height: _isContainerLarge==false? 100: 200,
+                width: _isContainerLarge==false? 100: 200,
+                color: Colors.red,
+                duration: Duration(seconds: 2),
+
+              ),
+            )
+          ]
       ),
     );
   }
